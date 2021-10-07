75c0f0a7b584ad75996c0da7c4e1ab078cbc2c02
JABV97 <jorgebv_97@hotmail.com>
2021-10-07 11:02:50 -0600

    cambios 07/10/2021

--- Kermesse Proyecto/App_Start/RouteConfig.cs
+++ Kermesse Proyecto/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Kermesse_Proyecto
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "CategoriaGastos", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
